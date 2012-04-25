48372d09b088b00a46fd13cdecfaa349f7287364
David McReynolds <dave@thedaylightstudio.com>
2012-04-24 23:48:05 -0700

    Added search install schema

---
+++ install/fuel_search_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `fuel_search` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `location` varchar(255) NOT NULL DEFAULT '',
+  `scope` varchar(50) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `date_added` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `location` (`location`),
+  FULLTEXT KEY `title` (`title`,`content`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
