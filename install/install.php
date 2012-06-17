6e7fb2f4fc3a99095564edb1ae3c810dff230690
David McReynolds <dave@thedaylightstudio.com>
2012-06-17 09:27:01 -0700

    Added readme and install SQL

---
+++ install/install.php
@@ -0,0 +1,16 @@
+<?php 
+$config['name'] = 'Search Module';
+$config['version'] = SEARCH_VERSION;
+$config['author'] = 'David McReynolds';
+$config['company'] = 'Daylight Studio';
+$config['license'] = 'Apache 2';
+$config['copyright'] = '2012';
+$config['author_url'] = 'http://www.thedaylightstudio.com';
+$config['description'] = 'The FUEL Search Module can be used create a search index to query against for your site.';
+$config['compatibility'] = '1.0';
+$config['instructions'] = '';
+$config['permissions'] = 'search';
+$config['migration_version'] = 0;
+$config['install_sql'] = 'fuel_search_install.sql';
+$config['uninstall_sql'] = 'fuel_search_uninstall.sql';
+$config['repo'] = 'git://github.com/daylightstudio/FUEL-CMS-Search-Module.git';
