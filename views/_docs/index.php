080c989be7c3447c40519f399f89a90509dde962
David McReynolds <dave@thedaylightstudio.com>
2012-09-21 06:56:12 -0700

    updated docs to include sitemap for search

--- views/_docs/index.php
+++ views/_docs/index.php
@@ -4,6 +4,11 @@
 <h2>Overview</h2>
 <p>The Search module can be used to index the content of your site and provides views for you to customize for your own search result pages.</p>
 
+<p>Additionall, a route can be created to generate a sitemap based on the search index like so:</p>
+<pre class="brush: php">
+$route['sitemap.xml'] = 'search/sitemap';
+</pre>
+
 <?=generate_config_info()?>
 
 <?=generate_toc()?>
