421cb4a5d1305b574f31c86f54c754fdb171d5e8
David McReynolds <dave@thedaylightstudio.com>
2013-02-25 11:27:04 -0800

    updated install permission

--- install/install.php
+++ install/install.php
@@ -9,7 +9,7 @@ $config['author_url'] = 'http://www.thedaylightstudio.com';
 $config['description'] = 'The FUEL Search Module can be used create a search index to query against for your site.';
 $config['compatibility'] = '1.0';
 $config['instructions'] = '';
-$config['permissions'] = 'tools/search';
+$config['permissions'] = array('tools/search' => 'Search');
 $config['migration_version'] = 0;
 $config['install_sql'] = 'fuel_search_install.sql';
 $config['uninstall_sql'] = 'fuel_search_uninstall.sql';
