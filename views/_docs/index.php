09a52de46806da1c4e7db6530c638bc55eb5c1a9
David McReynolds <dave@thedaylightstudio.com>
2014-01-12 16:54:50 -0800

    fixed typo in documentation

--- views/_docs/index.php
+++ views/_docs/index.php
@@ -4,7 +4,7 @@
 <h2>Overview</h2>
 <p>The Search module can be used to index the content of your site and provides views for you to customize for your own search result pages.</p>
 
-<p>Additionall, a route can be created to generate a sitemap based on the search index like so:</p>
+<p>Additionally, a route can be created to generate a sitemap based on the search index like so:</p>
 <pre class="brush: php">
 $route['sitemap.xml'] = 'search/sitemap';
 </pre>
